37213f2e4f7c6e22bc1dad51a90e1ddb81ffbdd2
Lukasz Swiatek <lukasz.swiatek@idemia.com>
2024-08-02 18:31:45 +0200

    unit tests

--- tests/FizzBuzz_tests.py
+++ tests/FizzBuzz_tests.py
@@ -1,7 +1,24 @@
+def fizzbuzz(n):
+    if n % 3 == 0 and n % 5 == 0:
+        return "FizzBuzz"
+    if n % 3 == 0:
+        return "Fizz"
+    elif n % 5 == 0:
+        return "Buzz"
+    else:
+        return n
 
 
-def fizzbuzz(n: int):
-    pass
 
 def test_returnsNumber():
     assert fizzbuzz(1) == 1
+    assert fizzbuzz(2) == 2
+    assert fizzbuzz(3) == "Fizz"
+    assert fizzbuzz(15) == "FizzBuzz"
+
+
+def test_returnsBuzz():
+    assert fizzbuzz(5) == "Buzz"
+    assert fizzbuzz(25) == "Buzz"
+    assert fizzbuzz(100) == "Buzz"
+
